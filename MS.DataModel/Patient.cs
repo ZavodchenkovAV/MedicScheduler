0f9c2a537455c04a7978a31b9bd5a3779bbb3b52
Алексей Владимирович Заводченков <statistzav@gmail.com>
2016-08-05 01:19:08 +0300

    datamodel stub and wpfclient stub

---
+++ MS.DataModel/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.DataModel
+{
+    public class Patient
+    {
+        public long PolicyId { get; set; }
+        public long Surname { get; set; }
+        public long Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
