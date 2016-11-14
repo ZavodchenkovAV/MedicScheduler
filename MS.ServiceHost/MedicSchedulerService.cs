55d0999b38dc067df73a22787c2c7fb05805a195
Алексей Владимирович Заводченков <statistzav@gmail.com>
2016-11-14 22:52:36 +0300

    wcf service implementation

---
+++ MS.ServiceHost/MedicSchedulerService.cs
@@ -0,0 +1,18 @@
+﻿using MS.DataModel;
+using MS.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.ServiceHost
+{
+    public class MedicSchedulerService: IMedicSchedulerService
+    {
+        public List<Appointment> GetAppointmentsByPatient(long? patientId)
+        {
+            return null;
+        }
+    }
+}
---
+++ MS.ServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using MS.ServiceContracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.ServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new System.ServiceModel.ServiceHost(typeof(MedicSchedulerService), new Uri("http://localhost:8000/MedicSchedulerService"));
+            host.AddServiceEndpoint(typeof(IMedicSchedulerService), new BasicHttpBinding(), "");
+            host.Open();
+            Console.WriteLine("Сервер запущен");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
